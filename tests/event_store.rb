fead86defd5833e01b6e44fbe738b89694c9f2e5
ntl <nathanladd+github@gmail.com>
2015-09-02 11:28:01 -0500

    Demonstrate reads

--- tests/event_store.rb
+++ tests/event_store.rb
@@ -22,6 +22,15 @@ def write_event_request
   request = HTTPProtocol::Request.new "POST", "/streams/testStream-#{uuid}"
   request.merge_headers common_headers
   request["Content-Type"] = "application/json"
+  request["ES-EventType"] = "TestEvent"
+  request["ES-EventId"] = uuid
+  request
+end
+
+def read_events_request
+  request = HTTPProtocol::Request.new "GET", "/streams/$ce-testStream/0/forward/50"
+  request.merge_headers common_headers
+  request["Accept"] << "application/json"
   request
 end
 
@@ -29,15 +38,12 @@ def establish_connection
   TCPSocket.new "127.0.0.1", "2113"
 end
 
-request = write_event_request
+describe "Writing events" do
+  tcp_socket = establish_connection
+  request = write_event_request
 
-tcp_socket = establish_connection
-
-describe "writing events" do
-  request["ES-EventType"] = "TestEvent"
   event = JSON.pretty_generate data: "a_msg"
 
-  request["ES-EventId"] = uuid
   request["Content-Length"] = event.size
 
   logger.debug do "Writing event:\n\n#{request}\r\n#{event}\r\n" end
@@ -54,4 +60,22 @@ describe "writing events" do
     tcp_socket.close
     tcp_socket = establish_connection
   end
+end
+
+describe "Reading events" do
+  tcp_socket = establish_connection
+  request = read_events_request
+
+  logger.debug do "Reading stream:\n\n#{request}\r\n" end
+  tcp_socket.write request
+
+  builder = HTTPProtocol::Response.builder
+  builder << tcp_socket.gets until builder.finished_headers?
+  response = builder.message
+  logger.debug do "Received response:\n\n#{response}" end
+
+  length = response["Content-Length"].to_i
+  data = tcp_socket.read length
+
+  logger.debug "Stream data:\n\n#{data}"
 end
